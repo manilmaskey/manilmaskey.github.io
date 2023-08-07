3305c2aaa3a3351737e60957f4f249e567023565
manilmaskey <manil.maskey@gmail.com>
2023-08-06 21:08:54 -0500

    New Theme

---
+++ public/sidebar.js
@@ -0,0 +1,33 @@
+document.getElementById("mysidebar").innerHTML = 
+    '<nav id="menu">'+
+        '<header class="major">'+
+            '<h2>Menu</h2>'+
+        '</header>'+
+        '<ul>'+
+            '<li><a href="/">Homepage</a></li>'+
+            '<li><a href="/#publications">Publications</a></li>'+
+            '<li><a href="/#workshop">Workshops</a></li>'+
+            '<li><a href="/#competition">Competitions</a></li>'+
+            '<li><a href="/#datasets">Datasets</a></li>'+
+            '<li><a href="/#outreach">Outreach</a></li>'+
+            '<!--'+
+            '<li><a href="generic.html">Generic</a></li>'+
+            '<li><a href="elements.html">Elements</a></li>'+
+            '<li>'+
+                '<span class="opener">Submenu</span>'+
+                '<ul>'+
+                    '<li><a href="#">Lorem Dolor</a></li>'+
+                    '<li><a href="#">Ipsum Adipiscing</a></li>'+
+                    '<li><a href="#">Tempus Magna</a></li>'+
+                    '<li><a href="#">Feugiat Veroeros</a></li>'+
+                '</ul>'+
+            '</li>'+
+            '-->'+
+        '</ul>'+
+    '</nav>'+
+
+
+    '<footer id="footer">'+
+        '<p class="copyright">&copy; Manil Maskey, 2023.<br />'+
+        'Website Template: <a href="https://html5up.net">HTML5 UP</a>.</p>'+
+    '</footer>';
